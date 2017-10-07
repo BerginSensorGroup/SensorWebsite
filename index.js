1b3137b0cceb3eacf7a8152e1285b58d368c61cf
Steven Yang <sgy2@duke.edu>
2017-10-07 16:37:18 -0400

    test connection to AWS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.set('port', process.env.PORT || 3000);
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World Test!')
 })
 
 app.listen(app.get('port'), function () {
