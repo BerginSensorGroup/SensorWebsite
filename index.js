6a99f6ede467b6fb249b357ca4fd78cde05e2dc3
Daniel Wu <dw352934@gmail.com>
2017-11-03 15:16:42 -0400

    Changed some stuff

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const model = require('/model.js')
+const controller = require('/controller.js')
+
 const app = express()
-app.set('port', process.env.PORT || 3000);
-app.get('/', function (req, res) {
-  res.send('Hello World Test User!')
-})
-app.get('/target', function (req, res) {
-  res.send('Target!')
-})
+	app.set('port', process.env.PORT || 3000);
+	app.use('/', function (req, res) {
+			res.send('Hello Chris!')
+			})
+
+app.use('/target', function (req, res) {
+
+		res.send('Target!')
+		})
+
+app.use('/air',  function(req, res){
+
+		res.send(200);
+		})
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port 3000!')
-})
+		console.log('Example app listening on port 3000!');
+		})
