bbde4b44e1d0af28d225576c6dc0e05a216b7a2b
stevenyang9 <sgyang9@gmail.com>
2017-11-03 20:44:33 -0400

    connect to mysql

--- controller.js
+++ controller.js
@@ -2,9 +2,10 @@ var mysql = require('mysql')
 
 module.exports = {
   database: function(){
-    var mysql      = require('mysql');
+    var mysql = require('mysql');
     var connection = mysql.createConnection({
-      host     : 'ec2-52-70-166-16.compute-1.amazonaws.com',
+      //host     : 'ec2-52-70-166-16.compute-1.amazonaws.com',
+      host     : 'berginlabdb.cyatkbygf3ox.us-east-1.rds.amazonaws.com',
       port     : '3306',
       user     : 'berginlab',
       password : 'Mike*sandals'
