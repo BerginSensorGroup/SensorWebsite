9d205625b662129588745169048a33bb1b353a3f
Daniel Wu <dw352934@gmail.com>
2018-02-09 01:31:27 -0500

    Finished search page for data

--- controllers/data/csv/index.js
+++ controllers/data/csv/index.js
@@ -3,7 +3,28 @@ const air = require('../../../models/air.js')
 let router = express.Router()
 
 router.get('/', function(req,res){
-    res.render('index')
+    res.render('data')
+})
+
+router.get('/:deviceid', function (req, res) {
+    console.log("Device Requested: " + req.params.deviceid);
+    var device_id = req.params.device_id;
+
+	var new_params ={
+		begin_date:req.query.begin_date,
+		end_date:req.query.end_date,
+		begin_event:req.query.begin_event,
+		end_event:req.query.end_event
+	}
+    air.getDevice(req.params.deviceid, new_params, function (err, results) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+           res.render('index')
+          // res.download('')
+        }
+    })
 })
 
 module.exports = router
