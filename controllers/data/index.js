279b7497590d0e51e62db2b7cd0204d2e9fcb0e3
Daniel Wu <dw352934@gmail.com>
2018-02-18 23:33:40 -0500

    Added different pages for projects and devices

--- controllers/data/index.js
+++ controllers/data/index.js
@@ -8,20 +8,31 @@ const csv = require('./csv')
 let router = express.Router()
 
 router.get('/', function (req, res) {
-    res.render('device')
+    res.render('overview-data')
 })
 
 router.post('/', function (req, res) {
-    if (req.body.data_type == "Graphical") {
-        return res.redirect(url.format({
+    if (req.body.data_type == "Graphical") 
+    {
+        return res.redirect(url.format(
+        {
             pathname: "/data/chart/" + req.body.device_id,
             query: _.pick(req.body, _.identity)
         }))
     }
-    else {
-        return res.redirect(url.format({
+    else if(req.body.data_type == "Download-CSV")
+    {
+        return res.redirect(url.format
+        ({
             pathname: "/data/csv/download/" + req.body.device_id,
             query: _.pick(req.body, _.identity)
+        }))
+    }
+    else{
+        return res.redirect(url.format
+        ({
+                pathname: "/data/csv/upload/" + req.body.device_id,
+                query: _.pick(req.body, _.identity)
         }))
     }
 })
